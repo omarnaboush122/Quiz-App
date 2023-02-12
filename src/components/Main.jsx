853f46a12a24aedf022ecd7058a83b03029bc0ff
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-11 20:42:41 -0800

    change questions data array

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,56 @@
 import { useEffect, useState } from "react";
-
+import { nanoid } from "nanoid";
 
 const Main = () => {
-  const [questionsData,setQuestionsData] = useState([]);
+  const [questionsData, setQuestionsData] = useState([]);
 
   const fetchData = async () => {
-    const res = await fetch("https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple");
+    const res = await fetch(
+      "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple"
+    );
     const data = await res.json();
-    setQuestionsData(data.results);
-  }
-
+    setQuestionsData(changeQuestionsArray(data.results));
+  };
 
   useEffect(() => {
     fetchData();
-  },[])
+  }, []);
 
   console.log(questionsData);
 
-  return (
-    <div className="main">
-      main
-    </div>
-  );
-}
+  const changeQuestionsArray = (array) => {
+    return array.map((item) => ({
+      question: item.question,
+      answers: [
+        {
+          incorrect_answer: item.incorrect_answers[0],
+          isHeld: false,
+          isTrue: false,
+          id: nanoid(),
+        },
+        {
+          incorrect_answer: item.incorrect_answers[1],
+          isHeld: false,
+          isTrue: false,
+          id: nanoid(),
+        },
+        {
+          incorrect_answer: item.incorrect_answers[2],
+          isHeld: false,
+          isTrue: false,
+          id: nanoid(),
+        },
+        {
+          correct_answer: item.correct_answer,
+          isHeld: false,
+          isTrue: true,
+          id: nanoid(),
+        },
+      ],
+    }));
+  };
+
+  return <div className="main">main</div>;
+};
 
 export default Main;
