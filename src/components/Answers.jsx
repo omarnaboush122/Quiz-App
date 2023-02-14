a275f58d4414ad266d8d3a8e7b0a4af9143ad583
omarnaboush122 <omarnaboush89@gmail.com>
2023-02-13 17:05:07 -0800

    add border to answers

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -2,22 +2,24 @@ import { useContext } from "react";
 import { Context } from "../Context";
 
 const Answers = ({ id, answer, isHeld, questionId, isCorrect }) => {
-  
   const { isChecked, isCorrect, handleAnswer } = useContext(Context);
 
   const style = () => {
     if (!isChecked) {
       return {
         backgroundColor: isHeld ? "#D6DBF5" : "#F5F7FB",
+        border: isHeld ? "1px solid #D6DBF5" : "1px solid #F5F7FB",
       };
     } else {
       if (isCorrect && isHeld) {
         return {
           backgroundColor: "#94D7A2",
+          border: isHeld && "1px solid #D6DBF5",
         };
       } else if (!isCorrect && isHeld) {
         return {
           backgroundColor: "#F8BCBC",
+          border: isHeld && "1px solid #D6DBF5",
         };
       }
     }
